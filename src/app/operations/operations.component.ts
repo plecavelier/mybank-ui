d0b4534eac19fdafeb7c69c1568986981f630aca
Pierre Lecavelier <Plecavelier@groupe361.local>
2016-09-05 22:51:30 +0200

    init operations table

--- src/app/operations/operations.component.ts
+++ src/app/operations/operations.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OperationService } from '../operation.service'
+import { AlertService } from '../alert.service'
+import { Operation } from '../operation'
+import { Alert } from '../alert'
 
 @Component({
   moduleId: module.id,
@@ -8,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperationsComponent implements OnInit {
 
-  constructor() { }
+  operations: Array<Operation>;
 
-  ngOnInit() {
+  constructor(
+    private operationService: OperationService,
+    private alertService: AlertService) {
   }
 
+  ngOnInit() {
+    this.operationService.operationChanged$.subscribe(
+      operations => this.refreshList()
+    );
+    this.refreshList();
+  }
+
+  private refreshList() {
+    this.operationService.getAll().subscribe(
+      operations => this.operations = operations,
+      error =>  this.alertService.emit(new Alert('danger', 'Une erreur est survenue durant la récupération des opérations'))
+    );
+  }
+
+  /*deleteAccount(account: Account) {
+    if (confirm('Souhaitez-vous vraiment supprimer ce compte bancaire ?')) {
+      this.accountService.delete(account).subscribe(
+        response => this.alertService.emit(new Alert('success', 'Le compte bancaire a bien été supprimé')),
+        error =>  this.alertService.emit(new Alert('danger', 'Une erreur est survenue durant la suppression du compte bancaire'))
+      );
+    }
+  }*/
 }
